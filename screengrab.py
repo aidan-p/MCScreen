5e0a0b6fb70faf2b592ec3ce6a7e942caa2838ca
Aidan Price <aidanjprice2004@gmail.com>
2025-05-29 22:22:28 -0400

    Reverting change; previous commit caused unstable framerates

--- screengrab.py
+++ screengrab.py
@@ -3,26 +3,38 @@ from PIL import Image
 import mss
 import numpy as np
 
-DESIRED_WIDTH = 64
-DESIRED_HEIGHT = 36
+# Change these values to the size you wish for your screen to be downscaled to
+# (Try to make it the same as your aspect ratio)
+desiredWidth = 64
+desiredHeight = 36
 
-BLOCK_NAMES = np.array(list(MINECRAFT_BLOCKS.keys()))
-BLOCK_COLORS = np.array(list(MINECRAFT_BLOCKS.values()), dtype=np.int32)  # Prevent overflow
+# Convert MINECRAFT_BLOCKS to NumPy array for fast color distance computation
+BLOCK_NAMES = list(MINECRAFT_BLOCKS.keys())
+BLOCK_COLORS = np.array(list(MINECRAFT_BLOCKS.values()))  # shape: (num_blocks, 3)
 
-def get_downscaled_screen(res=(DESIRED_WIDTH, DESIRED_HEIGHT)):
+def closest_block_color(rgb):
+    """Vectorized color distance computation."""
+    color = np.array(rgb)
+    distances = np.linalg.norm(BLOCK_COLORS - color, axis=1)
+    return BLOCK_NAMES[np.argmin(distances)]
+
+def get_downscaled_screen(res=(desiredWidth, desiredHeight)):
     with mss.mss() as sct:
-        monitor = sct.monitors[1]
+        monitor = sct.monitors[1]  # main monitor
         screenshot = sct.grab(monitor)
         img = Image.frombytes("RGB", (screenshot.width, screenshot.height), screenshot.rgb)
-        img = img.resize(res, Image.NEAREST)  # Fast resizing
+        img = img.resize(res, Image.NEAREST)
         return img
 
-def get_minecraft_block_map(res=(DESIRED_WIDTH, DESIRED_HEIGHT)):
+def get_minecraft_block_map(res=(desiredWidth, desiredHeight)):
     img = get_downscaled_screen(res)
-    np_pixels = np.asarray(img, dtype=np.int32)  # Prevent overflow
-    h, w = np_pixels.shape[:2]
-    flat_pixels = np_pixels.reshape(-1, 3)
-    dists = np.sum((flat_pixels[:, None, :] - BLOCK_COLORS[None, :, :]) ** 2, axis=2)
-    closest_indices = np.argmin(dists, axis=1)
-    block_names = BLOCK_NAMES[closest_indices]
-    return block_names.reshape(h, w).tolist()
+    img = img.convert("RGB")
+    np_pixels = np.array(img)  # shape: (height, width, 3)
+    height, width = np_pixels.shape[:2]
+
+    block_map = [
+        [closest_block_color(tuple(np_pixels[y, x])) for x in range(width)]
+        for y in range(height)
+    ]
+
+    return block_map
